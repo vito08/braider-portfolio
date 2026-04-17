48f74a46cfb099d0775f2124849e77487a342620
vito08 <vhzin.dev@gmail.com>
2026-04-17 00:06:18 -0300

    Atualizando links Whatsapp

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
 AOS.init({
   duration: 1000,
-  once: true
+  once: true,
+  disable: window.innerWidth < 768
 });
 
 window.addEventListener("scroll", () => {
   const nav = document.querySelector(".navbar");
   nav.classList.toggle("scrolled", window.scrollY > 50);
+});
+
+window.addEventListener('load', () => {
+  AOS.refreshHard();
 });
